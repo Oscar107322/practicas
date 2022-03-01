5b023a52a93e9b352300f0ccfd98333bf211893e
oscar <ramirez107322@insuco.edu.mx>
2022-03-01 10:02:45 -0600

    agregando examen

--- operadores.aremeticos.js
+++ operadores.aremeticos.js
@@ -12,3 +12,24 @@ C = A / b;
 console.log("Resultado de division es" + C)
 C = A % b;
 console.log("Resultado de operador demodelo que es el resultado  de una divicion es" + C)
+
+let j =10, o =32;
+let  z = j + o;
+//incremento
+//pre-incremento (el operacion ++ antes de la varianteble
+//z = ++j;
+console.log( j );
+console.log( z );
+//post-incremento(el operador ++ despues de la variable)
+//z = o ++;
+console.log( o );
+console.log( z );
+//Decremento
+//predecremento
+z = --j;
+console.log( j );
+console.log( z );
+//piostdecremento
+z =o --;
+console.log(o)
+console.log(z)
