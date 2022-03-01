f6136c23f98a8be4935513258356f13e2d4613e5
oscar <ramirez107322@insuco.edu.mx>
2022-03-01 10:57:49 -0600

    ejersicio1

---
+++ ejersicio1.js
@@ -0,0 +1,15 @@
+let A = 31, j = 26;
+let z = A * j;
+console.log( "Resultado de multiplicacion es:" + z );
+
+//Pre-incremnto
+//z = ++A;
+console.log( A )
+console.log( z )
+//Post-incremento
+//z = j++;
+console.log( j )
+console.log( z )
+o = ++z;
+console.log( o )
+console.log( z )
