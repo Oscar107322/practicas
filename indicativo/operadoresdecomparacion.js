5fd07294e41219424216140c4b636f9dff83b9d1
oscar <ramirez107322@insuco.edu.mx>
2022-03-03 11:15:38 -0600

    indicativoresuelto

---
+++ indicativo/operadoresdecomparacion.js
@@ -0,0 +1,6 @@
+let a =3,b=3,c="3"
+let z=a==c;//se revisa el valor sin impotar el tipo(a==c)
+//comparador estricto
+console.log( z )
+z =a===c; //revisa los valores son iguales pero tambien que los tiupos no sean iguales
+console.log( z )
