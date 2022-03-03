4ddac081370a5ac95072a03edf8975c7eddaab61
Javier Ramirez <travi_javier@hotmail..com>
2022-03-02 22:10:18 -0600

    Subiendo Parcial

---
+++ parcial/parcial.js
@@ -0,0 +1,16 @@
+/* Trabajo parcial
+
+Puntos a evaluar 
+
+1. Que sea limpio el codigo 
+2. Que no se le olvide el ;
+3. Se tomara en cuenta la logica de su codigo
+4. Que lo suban correctamente a github
+5. Que la sintaxis sea correcta
+
+Declarar 3 variables (a,b, y c) se les asignara un valor entero a la variable (a y b). A la variable (c) sele asignara un valor
+de dato tipo string y usando el operador distinto a (!=) y (!==).
+Utilizar un operador racional para saber si ( a o b ) es menor a (b). mostrarlo en un console.log()  
+
+
+*/
