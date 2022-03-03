42621ddc11c398c4d7ee19768f66dc91d47bc0ac
Javier Ramirez <travi_javier@hotmail..com>
2022-03-02 21:07:26 -0600

    Agregando indicativo

---
+++ indicativo/indicativo.js
@@ -0,0 +1,16 @@
+/* Trabajo de indicativo 
+
+Crear variables (a,b,c y d) hacer una suma de b y c dandole prioridad a la suma, despues hacer una multiplicacion 
+de del resultado de las variables b y c multiplicandola por d al ultimo sumar a y almacenar el resultado en la variable z.
+
+A la variale z se le incrementara el valor con el operador de asignacion (+=) se le sumara 4
+
+Puntos a evaluar 
+
+1. Que sea limpio el codigo 
+2. Que no se le olvide el ;
+3. Se tomara en cuenta la logica de su codigo
+4. Que lo suban correctamente a github
+5. Que la sintaxis sea correcta
+
+*/
