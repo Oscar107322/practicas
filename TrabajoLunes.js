6f4cbf7f4d99b2a4b07245c4f8e3b86bbd998737
Javier Ramirez <travi_javier@hotmail..com>
2022-03-06 14:30:27 -0600

    Ejercicio de lunes

---
+++ TrabajoLunes.js
@@ -0,0 +1,14 @@
+/*Ejercicio 1 : Utilizar el Operador distinto a != para saber si son diferentes.
+declar una variable para guardar el resultado y mostrarlo en console.log( ) */
+
+let a = 10;
+let b = 30;
+let c = 5;
+
+
+
+/*Ejercicio 2 : Preguntar si las variables son iguales o no a si para dar un resultado de verdadero o falso utilizando
+el operador de comparacion (==) y usando el operador de comparacion estricto (===).
+Declarar una variable donde almacenen el resultado y mostrarlo con un console.log()    */
+
+let d = 10, e = 56, f = 47, j = "10";
